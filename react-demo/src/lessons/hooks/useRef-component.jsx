afbf23a532d97a1d500c4c843f77f79ae5fad5a7
谭金涛1 <59766436+tjt740@users.noreply.github.com>
2022-05-31 01:18:51 +0800

    React.findNode()  React.creatRef()

--- react-demo/src/lessons/hooks/useRef-component.jsx
+++ react-demo/src/lessons/hooks/useRef-component.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 
 // step1️⃣ 从React中引入useRef函数
-import { useRef ,useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 
+
+class ClassCom extends React.Component {
+    render() {
+        return (
+            <>
+                <input placeholder="类组件" />
+                <h3>类组件</h3>
+            </>
+        )
+    }
+}
 
 function UseRefCom() {
     const h1Ref = useRef(null);
-    useEffect(() => { 
+    const classRef = useRef(null);
+    useEffect(() => {
         console.log(h1Ref);
         h1Ref.current.innerHTML = '哈哈哈'
         // todo: 可以尝试学习 给类组件绑定ref（不能给类组件绑定，可以封装个函数组件，函数组件引入类组件，然后给类组件绑定ref）
-    },[])
-    
+
+        // Tjt: React.findNode()  React.creatRef()
+
+        console.log(classRef)
+    }, [])
+
     return (
         <>
             <h1 ref={h1Ref}>123</h1>
-          
+            <ClassCom ref={ classRef } />
         </>
     )
 
